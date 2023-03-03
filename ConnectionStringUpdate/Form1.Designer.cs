991cbbea2e1811a3272bd24b98a964b8b5595070
yunuszemre <yunus.emre.teke@enoca.com>
2023-03-03 17:24:53 +0300

    Add project files.

---
+++ ConnectionStringUpdate/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ConnectionStringUpdate
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            radioDev = new RadioButton();
+            radioProd = new RadioButton();
+            radioTest = new RadioButton();
+            btnConvert = new Button();
+            panel1 = new Panel();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            panel2 = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // radioDev
+            // 
+            radioDev.AutoSize = true;
+            radioDev.Location = new Point(3, 25);
+            radioDev.Name = "radioDev";
+            radioDev.Size = new Size(96, 19);
+            radioDev.TabIndex = 2;
+            radioDev.TabStop = true;
+            radioDev.Text = "Development";
+            radioDev.UseVisualStyleBackColor = true;
+            // 
+            // radioProd
+            // 
+            radioProd.AutoSize = true;
+            radioProd.Location = new Point(126, 25);
+            radioProd.Name = "radioProd";
+            radioProd.Size = new Size(84, 19);
+            radioProd.TabIndex = 3;
+            radioProd.TabStop = true;
+            radioProd.Text = "Production";
+            radioProd.UseVisualStyleBackColor = true;
+            // 
+            // radioTest
+            // 
+            radioTest.AutoSize = true;
+            radioTest.Location = new Point(232, 25);
+            radioTest.Name = "radioTest";
+            radioTest.Size = new Size(45, 19);
+            radioTest.TabIndex = 4;
+            radioTest.TabStop = true;
+            radioTest.Text = "Test";
+            radioTest.UseVisualStyleBackColor = true;
+            // 
+            // btnConvert
+            // 
+            btnConvert.Location = new Point(305, 28);
+            btnConvert.Name = "btnConvert";
+            btnConvert.Size = new Size(271, 38);
+            btnConvert.TabIndex = 5;
+            btnConvert.Text = "Çevir";
+            btnConvert.UseVisualStyleBackColor = true;
+            btnConvert.Click += btnConvert_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(radioTest);
+            panel1.Controls.Add(radioDev);
+            panel1.Controls.Add(radioProd);
+            panel1.Location = new Point(7, 13);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(292, 61);
+            panel1.TabIndex = 6;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(6, 44);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(569, 232);
+            richTextBox1.TabIndex = 7;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(3, 319);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(569, 240);
+            richTextBox2.TabIndex = 8;
+            richTextBox2.Text = "";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(richTextBox1);
+            panel2.Controls.Add(richTextBox2);
+            panel2.Location = new Point(4, 80);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(585, 619);
+            panel2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 291);
+            label2.Name = "label2";
+            label2.Size = new Size(99, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Yeni Dosya İçeriği";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(136, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Değiştirilen Dosya İçeriği";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(591, 701);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(btnConvert);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TextBox txtFileName;
+        private Button btnFileSelect;
+        private RadioButton radioDev;
+        private RadioButton radioProd;
+        private RadioButton radioTest;
+        private Button btnConvert;
+        private Panel panel1;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private Panel panel2;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ ConnectionStringUpdate/Form1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace ConnectionStringUpdate
+{
+    public partial class Form1 : Form
+    {
+        string file = string.Empty;
+        string prodFile = string.Empty;
+        string testFile = string.Empty;
+        string devFile = string.Empty;
+        string solitonRoute = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+            solitonRoute = ConfigurationManager.AppSettings["solutionroute"];
+        }        
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo fileData = new DirectoryInfo(solitonRoute);
+            FileInfo[] files = fileData.GetFiles();
+
+            foreach (FileInfo item in files)
+            {
+
+                if (item.Name.Equals("appsettings.json"))
+                {
+                    file = item.FullName; break;
+                }
+            }
+            SourceFileSelect(files);
+
+            ControlRadio();
+        }
+
+        private void ControlRadio()
+        {
+            if (radioDev.Checked)
+            {
+                UpdateDatabaseConnectionString(file, devFile);
+            }
+            else if (radioProd.Checked)
+            {
+                UpdateDatabaseConnectionString(file, prodFile);
+            }
+            else
+            {
+                UpdateDatabaseConnectionString(file, testFile);
+            }
+        }
+
+        string SourceFileSelect(FileInfo[] files)
+        {
+
+
+
+            if (radioDev.Checked)
+            {
+                devFile = files.FirstOrDefault(x => x.Name == "appsettings.dev.json").FullName.ToString();
+                return devFile;
+            }
+            else if (radioProd.Checked)
+            {
+                prodFile = files.FirstOrDefault(x => x.Name == "appsettings.prod.json").FullName.ToString();
+                return prodFile;
+            }
+            else
+            {
+                testFile = files.FirstOrDefault(x => x.Name == "appsettings.test.json").FullName.ToString();
+                return testFile;
+            }
+
+
+        }
+        private void UpdateDatabaseConnectionString(string path, string sourcePath)
+        {
+            // Dosya var mı yok mu kontrol ediyoruz
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("Dosya bulunamadı");
+                return;
+            }
+            if (!File.Exists(sourcePath))
+            {
+                MessageBox.Show("Dosya bulunamadı");
+                return;
+            }
+            // Dosyadaki tüm metni okuyoruz
+            string targetJson = File.ReadAllText(path);
+
+            string sourceJson = File.ReadAllText(sourcePath);
+
+
+
+
+
+            // JSON dosyasını güncelliyoruz
+
+            File.WriteAllText(path, sourceJson);
+
+            richTextBox1.Text = targetJson;
+            richTextBox2.Text = sourceJson;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
